fbf51e341c5560a58d4fed27ae2722ec9914518e
Keval <kevalmehta885@gmail.com>
2025-02-04 19:05:58 +0530

    userSchema with password encrypt & compare feature builded

---
+++ backend/models/user.model.js
@@ -0,0 +1,68 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minLength: [6, "Password must be at least 6 characters long"],
+    },
+    cartItems: [
+      {
+        quantity: {
+          type: Number,
+          default: 1,
+        },
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+          required: [true, "Product is required"],
+        },
+      },
+    ],
+    role: {
+      type: String,
+      enum: ["customer", "admin"],
+      default: "customer",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// pre-hook to Hash the password before saving the user model database
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  } // Only hash if password is modified
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = bcrypt.hash(this.password, salt); // Hash with salt rounds of 10
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Match user entered password to hashed password in database
+// .methods is an object where you can define custom instance methods for your schema.
+userSchema.methods.comparePassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
