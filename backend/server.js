a7261c357da83bc5be8c2ec2379754d03499be19
Keval <kevalmehta885@gmail.com>
2025-02-04 18:09:20 +0530

    connecting with MongoDB via connectDB()

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
+import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 
@@ -14,4 +15,8 @@ app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+  connectDB();
 });
+
+//keval
+// 3PGfAq89ofy66t6r
