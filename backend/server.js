2fcb05696628d97e6b94f52f0db6b9ee7c20d860
Keval <kevalmehta885@gmail.com>
2025-02-04 17:39:53 +0530

    Route & controller Setup done

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 
@@ -7,9 +8,9 @@ const app = express();
 
 const PORT = process.env.PORT || 6500;
 
-app.get("/", (req, res) => {
-  res.send("Server is ready");
-});
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
