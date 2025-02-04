74f3b29f48928ed73e7ff78d40fe90a2401689ca
Keval <kevalmehta885@gmail.com>
2025-02-04 20:27:21 +0530

    solved bug

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -50,7 +50,7 @@ userSchema.pre("save", async function (next) {
   } // Only hash if password is modified
   try {
     const salt = await bcrypt.genSalt(10);
-    this.password = bcrypt.hash(this.password, salt); // Hash with salt rounds of 10
+    this.password = await bcrypt.hash(this.password, salt); // Hash with salt rounds of 10
     next();
   } catch (error) {
     next(error);
@@ -60,7 +60,7 @@ userSchema.pre("save", async function (next) {
 // Match user entered password to hashed password in database
 // .methods is an object where you can define custom instance methods for your schema.
 userSchema.methods.comparePassword = async function (enteredPassword) {
-  return await bcrypt.compare(enteredPassword, this.password);
+  return bcrypt.compare(enteredPassword, this.password);
 };
 
 const User = mongoose.model("User", userSchema);
