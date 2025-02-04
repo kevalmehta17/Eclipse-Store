20a6593b1bbb7e6dcefd090aa5d2c10a8a2df431
Keval <kevalmehta885@gmail.com>
2025-02-04 20:27:06 +0530

    signup & login endpoint builded

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,39 @@
-export const signup = async (req, res) => {};
+import User from "../models/user.model.js";
 
-export const login = async (req, res) => {};
+export const signup = async (req, res) => {
+  const { email, password, name } = req.body;
+  try {
+    const userAlreadyExist = await User.findOne({ email });
+    if (userAlreadyExist) {
+      return res
+        .status(400)
+        .json({ success: false, message: "User Already Exist" }); // 400 Bad Request
+    }
+    const user = await User.create({ name, email, password });
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+    });
+  } catch (error) {
+    console.error("Error: ", error);
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.find({ email });
+    if (!user) {
+      return res
+        .status(400)
+        .json({ success: false, message: "User not found" });
+    }
+    // if(user && (await user.comparePassword(password)))
+  } catch (error) {}
+};
 
 export const logout = async (req, res) => {};
