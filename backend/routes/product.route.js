731433e285a66306c9cadc1ce07d58ce67b17adf
Keval <kevalmehta885@gmail.com>
2025-02-14 19:34:09 +0530

    Cloudinary setup

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { protectRoute, adminRoute } from "../middleware/auth.middleware.js";
-import { getAllProducts } from "../controllers/product.controller.js";
+import { getAllProducts, getFeaturedProducts } from "../controllers/product.controller.js";
 
 const router = express.Router();
 
 router.get("/", protectRoute, adminRoute, getAllProducts);
 router.get("/featured", getFeaturedProducts);
+router.post("/", protectRoute, adminRoute, createProduct);
 
 export default router;
