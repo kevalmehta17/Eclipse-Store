980d348b8e2b75e6c7aff85768c4f138d01d662d
Keval <kevalmehta885@gmail.com>
2025-02-14 19:23:12 +0530

    Featured Product Controller & router created & fetched from redis/MongoDB

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -1,3 +1,4 @@
+import { redis } from "../lib/redis.js";
 import Product from "../models/product.model.js";
 
 export const getAllProducts = async (req, res) => {
@@ -9,3 +10,25 @@ export const getAllProducts = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+export const getFeaturedProducts = async (req, res) => {
+  try {
+    let featuredProducts = await redis.get("featured_products");
+    if (featuredProducts) {
+      return res.json(JSON.parse(featuredProducts))
+    }
+    //if not from redis then fetched from the mongoDB
+    //.lean() is used to get the plain JS object instead of mongoose object
+    featuredProducts = await Product.find({ isFeatured: true }).lean();
+    if (!featuredProducts) {
+      return res.status(404).json({ message: "Featured products not found" });
+    }
+    //store in redis for quick access
+    await redis.set("featured_products", JSON.stringify(featuredProducts));
+    res.json(featuredProducts);
+  } catch (error) {
+    console.log("Error in product controller", error.message);
+    res.status(500).json({ message: error.message });
+  }
+  //get the featuredData as object from redis and set as string
+}
--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -5,5 +5,6 @@ import { getAllProducts } from "../controllers/product.controller.js";
 const router = express.Router();
 
 router.get("/", protectRoute, adminRoute, getAllProducts);
+router.get("/featured", getFeaturedProducts);
 
 export default router;
