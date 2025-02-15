ac711152d7bb2cccfe8a0169567aefa2f5c63cfa
Keval <kevalmehta885@gmail.com>
2025-02-15 20:13:50 +0530

    lineItems, session & createStripeCoupon builded

--- backend/controllers/payment.controller.js
+++ backend/controllers/payment.controller.js
@@ -1,1 +1,63 @@
-export const createCheckoutSession = async (req, res) => { }
+import { stripe } from "../lib/stripe.js";
+import Coupon from "../models/coupon.model.js";
+
+export const createCheckoutSession = async (req, res) => {
+    try {
+        const { products, couponCode } = req.body;
+        if (!Array.isArray(products) || products.length === 0) {
+            return res.status(400).json({ message: "Invalid or empty products array" });
+        }
+        let totalAmount = 0;
+
+        const lineItems = products.map((product) => {
+            const amount = Math.round(product.price * 100);//stripe wants amount in cents
+            totalAmount += amount * product.quantity;
+            return {
+                price_data: {
+                    currency: "usd",
+                    product_data: {
+                        name: product.name,
+                        images: [product.image],
+                    },
+                    unit_amount: amount,
+                }
+            }
+        })
+        let coupon = null;
+        if (couponCode) {
+            coupon = await Coupon.findOne({ code: couponCode, userId: req.user._Id, isActive: true });
+
+            if (coupon) {
+                totalAmount -= Math.round(totalAmount * coupon.discountPercentage / 100);
+            }
+        }
+        const session = await stripe.checkout.sessions.create({
+            payment_method_types: ["card"],
+            line_items: lineItems,
+            mode: "payment",
+            success_url: `${process.env.CLIENT_URL}/purchase-success?session_id={CHECKOUT_SESSION_ID}`,
+            cancel_url: `${process.env.CLIENT_URL}/purchase-cancel`,
+            discounts: coupon ? [
+                {
+                    //this will decrease the total amount by discount percentage in Stripe
+                    coupon: await createStripeCoupon(coupon.discountPercentage),
+                }
+            ] : [],
+            metadata: {
+                userId: req.user._id.toString(),
+                couponCode: couponCode || "",
+
+            }
+        })
+    } catch (error) {
+
+    }
+}
+
+async function createStripeCoupon(discountPercentage) {
+    const coupon = await stripe.coupons.create({
+        percent_off: discountPercentage,
+        duration: "once"
+    })
+    return coupon.id;
+}
