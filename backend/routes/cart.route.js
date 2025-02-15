e616787a9b2528bc99e44f15a46821175dd2f213
Keval <kevalmehta885@gmail.com>
2025-02-15 12:30:38 +0530

    All Cart Routes builded

--- backend/routes/cart.route.js
+++ backend/routes/cart.route.js
@@ -1,7 +1,11 @@
 import express from "express";
 import { addToCart } from "../controllers/cart.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
-router.get("/", addToCart);
+router.get("/", protectRoute, getCartProducts);
+router.get("/", protectRoute, addToCart);
+router.delete("/", protectRoute, removeAllFromCart);
+router.put("/:id", protectRoute, updateQuantity);
 export default router;
